f1507b907e705d36fb4b3f606a9b91c5697967e0
mateus-henriquee <mateush.leccese@gmail.com>
2025-09-17 12:10:35 -0300

    a

---
+++ Assets/DungeonGame/Scripts/MenuInicial.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+   
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
